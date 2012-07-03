c66da9870735a43fe7b09c3dab7d6a03f038e567
Rebecca Lawler <rlawler@google.com>
2012-07-03 11:33:44 -0700

    Fixed UI tree issues, started adding AJAX function
    Modified Jstree settings to allow for clicking on a tree node's name,
    not just the checkbox. Added several functions in treehandler.js to
    allow for AJAX calls to a local server (getting ready for the UI to
    start talking to the backend)
    
    Change-Id: Ic58e3b955b33b1235981f8facd60ead4666e5d26

--- app/js/treehandler.js
+++ app/js/treehandler.js
@@ -8,6 +8,7 @@ var state1 = new Array(); // Contains the info of all things that are selected
 var state2 = new Array(); // All the things selected via checkbox in tree2
 var state3 = new Array(); // tree3 data
 
+
 // Here is some test data. 
 var treeModel = [
 {"data":"Confirm Application","attr":{"id":"10"},"children":null},{"data":"Things","attr":{"id":"20"},"children":[{"data":"Thing 1","attr":{"id":"21"},"children":null},{"data":"Thing 2","attr":{"id":"22"},"children":null},{"data":"Thing 3","attr":{"id":"23"},"children":null},{"data":"Thing 4","attr":{"id":"24"},"children":[{"data":"Thing 4.1","attr":{"id":"241"},"children":null},{"data":"Thing 4.2","attr":{"id":"242"},"children":null},{"data":"Thing 4.3","attr":{"id":"243"},"children":null}]}]},{"data":"Colors","attr":{"id":"40"},"children":[{"data":"Red","attr":{"id":"41"},"children":null},{"data":"Green","attr":{"id":"42"},"children":null},{"data":"Blue","attr":{"id":"43"},"children":null},{"data":"Yellow","attr":{"id":"44"},"children":null}]}];
@@ -15,7 +16,7 @@ var treeModel = [
 
 // The configuration for checkboxes.
 var treeConfig = {
-  core : {}, 
+  core : {},
   themes : {
     dots : false,
     icons : false, 
@@ -37,22 +38,30 @@ var treeConfig = {
   },
 };
 
-
+////////////////////////////////////////////////////////////////////////////////
 // A function to bind tree operations to events
-$(function (){ 
+function initTree(){
 
-  $("#clearer").click(function(){ 
+  // Clears out all trees
+  $("#clearer").click(function(){
     $("#treeView1").jstree("uncheck_all");
     state1 = []; // empty it out
     document.getElementById('tabs1').innerHTML = "<p>" + state1.toString() + "</p>";
-
     $("#treeView2").empty();
     state2 = [];
     document.getElementById('tabs2').innerHTML = "<p>" + state2.toString() + "</p>";
     $("#treeView3").empty();
-
+    state3 = [];
   });
 
+
+  // Triggered when a name is clicked, rather than a checkbox
+  // simply calls code below
+  $("#treeView1").bind("select_node.jstree", function (event, data) {
+    // `data.rslt.obj` is the jquery extended node that was clicked
+    data.inst.change_state(data.rslt.obj);
+    $("#treeView1").trigger('change_state.jstree', data); // yay!
+  });
 
   // The code to recognize a state change - when a checkbox changes
   $("#treeView1").bind("change_state.jstree", function (e, d) {
@@ -63,12 +72,12 @@ $(function (){
 
       // If there is a state change, we modify the state Array
       state1 = [];
- 
+
       // Get all selected nodes (by name)
-      $("#treeView1").jstree("get_checked").each(function(key, value){ 
-        state1.push($(value).children("a").text()); 
-      }); 
-      
+      $("#treeView1").jstree("get_checked").each(function(key, value){
+        state1.push($(value).children("a").text());
+      });
+
       document.getElementById('tabs1').innerHTML = "<p>" + state1.toString() + "</p>";
 
       // We add or remove the second tree as necessary
@@ -76,8 +85,6 @@ $(function (){
 
         if (state2.length == 0) {
           $("#treeView2").jstree(treeConfig);
-
-          // We bind the change_state handler each time the tree is created
           $("#treeView2").bind("change_state.jstree", function (e, d) {
             var tagName = d.args[0].tagName;
             var refreshing = d.inst.data.core.refreshing;
@@ -85,21 +92,44 @@ $(function (){
               (refreshing != true && refreshing != "undefined")) {
 
               state2 = [];
- 
-              $("#treeView2").jstree("get_checked").each(function(key, value){ 
-                state2.push($(value).children("a").text()); 
-              }); 
-      
+
+              $("#treeView2").jstree("get_checked").each(function(key, value){
+                state2.push($(value).children("a").text());
+              });
+
               document.getElementById('tabs2').innerHTML = "<p>" + state2.toString() + "</p>";
 
-              // We add or remove the next tree as necessary
+              // We add or remove the third tree as necessary
               if (state2.length > 0){
-                $("#treeView3").jstree(treeConfig);
+
+                if (state3.length == 0) {
+                  $("#treeView3").jstree(treeConfig);
+                  $("#treeView3").bind("change_state.jstree", function (e, d) {
+                    var tagName = d.args[0].tagName;
+                    var refreshing = d.inst.data.core.refreshing;
+                    if ((tagName == "A" || tagName == "INS") &&
+                      (refreshing != true && refreshing != "undefined")) {
+                      state3 = [];
+                      $("#treeView3").jstree("get_checked").each(function(key, value){
+                        state3.push($(value).children("a").text());
+                      });
+                    }
+                  });
+                  $("#treeView3").bind("select_node.jstree", function (event, data) {
+                    data.inst.change_state(data.rslt.obj);
+                    $("#treeView3").trigger('change_state.jstree', data); // yay!
+                  });
+                }
               }
-              else { // We need to destroy the next tree (if exists)
+              else { // We need to destroy the third tree (if exists)
                 $("#treeView3").empty();
+                state3 = [];
               }
             }
+          });
+          $("#treeView2").bind("select_node.jstree", function (event, data) {
+            data.inst.change_state(data.rslt.obj);
+            $("#treeView2").trigger('change_state.jstree', data); // yay!
           });
 
         }
@@ -109,17 +139,19 @@ $(function (){
         state2 = [];
         document.getElementById('tabs2').innerHTML = "<p>" + state2.toString() + "</p>";
         $("#treeView3").empty();
+        statd3 = [];
       }
     }
   });
 
-});
+};
 
 
 // We create tree1 and the tabs to start (others will be added as we go along).
 $(document).ready(function () {
   $("#treeView1").jstree(treeConfig);
   $( "#tabs" ).tabs({ fx: { height: 'toggle', opacity: 'toggle'} });
+  initTree();
 });
 
 
