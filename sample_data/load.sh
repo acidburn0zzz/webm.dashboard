86f82212b9695d3c3998249c8ef5d98ec3007fbe
John Koleszar <jkoleszar@google.com>
2012-08-16 14:25:21 -0700

    load.sh: add support for changing hosts
    Set the HOST environment variable to upload the data to a production
    server.
    
    Change-Id: I2372591fe9a54244a5076eade876efc1a375e7f7

--- sample_data/load.sh
+++ sample_data/load.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 self=${0}
 dir=$(dirname $self)
-for f in ${1:-${dir}/*.json}; do
+HOST=${HOST:-localhost:8080}
+for f in ${@:-${dir}/*.json}; do
     echo "Loading $f ..."
     case "$f" in
-      *commits.json)
-         curl -F data=@${f} http://localhost:8080/gerrit/import-commits
+      *commits*.json)
+         curl -F data=@${f} http://$HOST/gerrit/import-commits
          ;;
       *filesets.json)
-         curl -F data=@${f} http://localhost:8080/import-filesets
+         curl -F data=@${f} http://$HOST/import-filesets
          ;;
       *metrics.json)
-         curl -F data=@${f} http://localhost:8080/import-metrics
+         curl -F data=@${f} http://$HOST/import-metrics
          ;;
       *)
-         curl -F data=@${f} http://localhost:8080/import-codec-metrics
+         curl -F data=@${f} http://$HOST/import-codec-metrics
          ;;
     esac
 done
